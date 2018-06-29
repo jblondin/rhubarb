bc334bf75655599d8c9163f9e76ad224ca20be2a
Jamie Blondin <jblondin@gmail.com>
2018-06-29 18:43:13 -0400

    Fixed rhubarb-graph test compile error

--- rhubarb-graph/tests/scatter_serialize.rs
+++ rhubarb-graph/tests/scatter_serialize.rs
@@ -1,58 +1,46 @@
-#[macro_use] extern crate df;
+extern crate agnes;
 extern crate serde;
 extern crate serde_json;
 extern crate rhubarb_graph as rg;
 
+use rg::scatter::Scatter;
 use std::path::Path;
 
-use df::source::{CsvReader, CsvSourceBuilder, FileSource};
-use df::field::{FieldIdent, FieldType};
-use df::view::DataView;
+use agnes::source::{CsvReader, CsvSource};
+use agnes::view::DataView;
 
 #[test]
 fn scatter_serialize() {
     let data_filepath = Path::new("../").join(
         Path::new(file!()).parent().unwrap().join("data/sample1.csv")
     );
-    let file = FileSource::new((&data_filepath).into());
-    let mut csv_rdr = CsvReader::new(
-        CsvSourceBuilder::default()
-            .file_source(file)
-            .fields(fields![
-                "state" => FieldType::Text,
-                "val1" => FieldType::Unsigned,
-                "val2" => FieldType::Float
-            ])
-            .build().unwrap()
-    ).unwrap();
-    let dv: DataView = csv_rdr.read().unwrap().into();
+    let source = CsvSource::new((&data_filepath).into()).unwrap();
+    let mut csv_rdr = CsvReader::new(&source).unwrap();
+    let dv: DataView = DataView::from(csv_rdr.read().unwrap()).v(["state", "val1", "val2"]);
 
     println!("{}", dv);
     println!("{}", serde_json::to_string(&dv).unwrap());
-    let scatter = rg::ScatterBuilder::default()
-        .x(&dv%"val1")
-        .y(&dv%"val2")
-        .build().unwrap();
+    let scatter = Scatter::new(
+        dv.v("val1"),
+        dv.v("val2")
+    );
     println!("{}", serde_json::to_string(&scatter).unwrap());
 
-    let graph: rg::Graph<rg::Scatter> = rg::GraphBuilder::default()
-        .data(vec![scatter])
-        .layout(rg::LayoutBuilder::default()
+    let graph: rg::Graph<Scatter> = rg::Graph::new(
+        vec![scatter],
+        rg::Layout::default()
             .title("Sample Scatter Plot!")
             .width(600usize)
             .height(500usize)
-            .x_axis(rg::AxisBuilder::default()
+            .x_axis(rg::common::Axis::default()
                 .title("Value 1")
-                .kind(rg::AxisKind::Log)
-                .build().unwrap()
+                .kind(rg::common::AxisKind::Log)
             )
-            .y_axis(rg::AxisBuilder::default()
+            .y_axis(rg::common::Axis::default()
                 .title("Value 2")
-                .kind(rg::AxisKind::Linear)
-                .build().unwrap()
+                .kind(rg::common::AxisKind::Linear)
             )
-            .margin(rg::Margin::from_hv(10, 10))
-            .build().unwrap()
-        ).build().unwrap();
+            .margin(rg::common::Margin::from_hv(10, 10))
+    );
     println!("{}", serde_json::to_string_pretty(&graph).unwrap());
 }
